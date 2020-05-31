8b7fd51aeb8bf6d475417d43800f405324381cf5
Chris <63784760+chk-code@users.noreply.github.com>
2020-05-31 12:34:07 +0200

    add: additional features

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
+const apiId = 'c2309dnmz9'
 const awsRegion = 'eu-central-1'
 export const apiEndpoint = `https://${apiId}.execute-api.${awsRegion}.amazonaws.com/dev`
 
