dbc0bea9fd8b48322c2369c6cf6907d6886c08a8
Chris <63784760+chk-code@users.noreply.github.com>
2020-06-02 12:13:53 +0200

    fix: current postman collection and config.ts for PROD

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,7 +1,8 @@
 // DONE: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'el4w1byr5i'
+const apiId = 'oye9ivnfag'
+const stageName = 'prod'
 const awsRegion = 'eu-central-1'
-export const apiEndpoint = `https://${apiId}.execute-api.${awsRegion}.amazonaws.com/dev`
+export const apiEndpoint = `https://${apiId}.execute-api.${awsRegion}.amazonaws.com/${stageName}`
 
 export const authConfig = {
   // DONE: Create an Auth0 application and copy values from it into this map
